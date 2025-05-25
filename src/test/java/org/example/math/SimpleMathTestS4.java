9deb1fe3a8875536e097232c5cd772f2b8a02524
Vinicius Caixeta <vinis_caixetabaixo@hotmail.com>
2025-05-25 19:49:45 -0300

    Testes usando csv

--- src/test/java/org/example/math/SimpleMathTestS4.java
+++ src/test/java/org/example/math/SimpleMathTestS4.java
@@ -3,6 +3,8 @@ package org.example.math;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -20,10 +22,21 @@ class SimpleMathTestS4 {
         math = new SimpleMath();
     }
 
-    @DisplayName("Test 6.2 / 2 = 3.1")
+    @DisplayName("Test double subtraction [firstNumber, secondNumber, expected]")
     @ParameterizedTest
     //@MethodSource("testDivisionInputParameters")
-    @MethodSource()
+    //@MethodSource()
+    /*@CsvSource({
+            "6.2, 2, 3.1",
+            "71, 14, 5.07",
+            "18.3, 3.1, 5.90"
+    })
+    @CsvSource({
+            "Pelé, Football",
+            "Senna, F1",
+            "Keith Moon, ''"
+    })*/
+    @CsvFileSource(resources = "/testDivision.csv")
     void testDivision(double firstNumber, double secondNumber, double expected) {
 
         System.out.println("Test " + firstNumber + " / " + secondNumber + " = " + expected + "!");
