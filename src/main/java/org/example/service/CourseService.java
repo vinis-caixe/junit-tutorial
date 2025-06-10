dc24f0cac55775516d080b3b8902b2d7b7a3bd5c
Vinicius Caixeta <vinis_caixetabaixo@hotmail.com>
2025-06-10 20:10:06 -0300

    Iniciando Mockito

---
+++ src/main/java/org/example/service/CourseService.java
@@ -0,0 +1,9 @@
+package org.example.service;
+
+import java.util.List;
+
+public interface CourseService {
+
+    public List<String> retrieveCourses(String student);
+    public List<String> doSomething(String student);
+}
---
+++ src/test/java/org/example/business/CourseBusinessTest.java
@@ -0,0 +1,37 @@
+package org.example.business;
+
+import org.example.service.CourseService;
+import org.example.service.stub.CourseServiceStub;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseBusinessTest {
+
+    @Test
+    void testCoursesRelatedToSpring_When_UsingAStub() {
+        // Given
+        CourseService stubService = new CourseServiceStub();
+        CourseBusiness business = new CourseBusiness(stubService);
+
+        // When
+        var filteredCourses = business.retrieveCoursesRelatedToSpring("Leandro");
+
+        // Then
+        assertEquals(4, filteredCourses.size());
+    }
+
+    @Test
+    void testCoursesRelatedToSpring_When_UsingAFooBarStub() {
+        // Given
+        CourseService stubService = new CourseServiceStub();
+        CourseBusiness business = new CourseBusiness(stubService);
+
+        // When
+        var filteredCourses = business.retrieveCoursesRelatedToSpring("Foo Bar");
+
+        // Then
+        assertEquals(0, filteredCourses.size());
+    }
+
+}
